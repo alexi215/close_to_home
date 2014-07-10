5803067725a08f1280802fc9da6af71f65c8ab63
alexi215 <alexi83@gmail.com>
2014-07-10 18:23:42 -0400

    created tables and ran migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
-
+  root 'intro#index'
+  
+  
 end
--- db/migrate/20140710204347_create_locations.rb
+++ db/migrate/20140710204347_create_locations.rb
@@ -1,7 +1,11 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-      
+      t.string :label
+      t.string :address, null: false
+      t.integer :radius, :float, default: '.25'
+      t.string :lat, :decimal
+      t.string :long, :decimal
     end
   end
 end
