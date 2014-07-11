61f862897affac09fbce681d08f5b6878d2c22c9
alexi215 <alexi83@gmail.com>
2014-07-11 10:35:54 -0400

    updated routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  root 'intro#index'
-  
+  root 'welcome#index'
+
+  resources :users, except: [:index]
+  resources :sessions, only: [:create]
+
+  get '/singup' => 'sessions#new'
+  get '/signin' => 'sessions#destroy'
+  delete '/signout' => 'sessions#destroy'
   
 end
