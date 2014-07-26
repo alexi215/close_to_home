19376d6251e71bce7b0ac1dc5081b8dfb35a721a
alexi215 <alexi83@gmail.com>
2014-07-26 14:46:15 -0400

    changed logic to crimes controller

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,12 +1,11 @@
 class CrimesController < ApplicationController
 
-  def extract
-    response = HTTParty.get('http://data.octo.dc.gov/feeds/crime_incidents/crime_incidents_current.xml')
-    # Hash of get request
-    data = response.parsed_response
-    # Extracting hash to create array of recent crimes
-    crimes_array = data['feed']['entry']
-    return crimes_array.to_json
+  def index
+    @crimes = extract_crimes
+      respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @crimes }
+    end
   end
 
 end # END of crimes controller
