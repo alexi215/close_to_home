46aac58bf4d3a2c67ee8316c3e3a3f3b33fe9965
alexi215 <alexi83@gmail.com>
2014-08-04 23:06:33 -0400

    crimes might working - over query limit

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,8 +1,17 @@
 class Crime < ActiveRecord::Base
 
+  # validates :latitude, :longitude, :allow_blank => false, :allow_nil => false
   validates_uniqueness_of :date, scope: [:address, :offense]
-  
+
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, on: :create
+  after_validation :geocode, :if => :address_not_converted
+
+  def address_not_converted
+    Crime.all do |crime|
+      crime[:latitude] == nil && crime[:longitude] == nil
+    end
+  end
 
 end # END of class
+
