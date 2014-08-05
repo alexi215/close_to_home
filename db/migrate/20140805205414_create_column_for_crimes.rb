27e40ca14d584d43a1f7561e666c27f4d50173e5
alexi215 <alexi83@gmail.com>
2014-08-05 17:19:06 -0400

    deleted old migration files

--- db/migrate/20140805205414_create_column_for_crimes.rb
+++
@@ -1,7 +0,0 @@
-class CreateColumnForCrimes < ActiveRecord::Migration
-  def change
-    create_table :column_for_crimes do |t|
-      t.string "db_time"
-    end
-  end
-end
--- db/migrate/20140805210952_drop_delayed_jobs.rb
+++
@@ -1,6 +0,0 @@
-class DropDelayedJobs < ActiveRecord::Migration
-  def change
-    drop_table :delayed_jobs
-    drop_table :column_for_crimes
-  end
-end
